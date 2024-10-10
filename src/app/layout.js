49212b7bc2de8915a497a6421c14d18cc300b76f
Marlon <marlybone@gmail.com>
2024-10-10 12:43:07 +0200

    minor edits

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Navbar } from "./Components/Navbar";
 import { Footer } from "./Components/Footer";
 import { DonationProvider } from "./Components/donatecontext";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} dark:bg-neutral-900 antialiased max-w-5xl mx-auto px-4 bg-white`}
       >
